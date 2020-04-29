25c240d96afea6d971b6f79dc0013750cd99394e
Jordan Giustizia <33008862+JordanGiustizia@users.noreply.github.com>
2020-04-28 21:42:52 -0400

    Elementary navigation functionality

--- index.js
+++ index.js
@@ -3,59 +3,46 @@ const context = canvas.getContext('2d');
 let canvasWidth = window.innerWidth;
 let canvasHeight = window.innerHeight;
 
-const getScrollbarWidth = function() {
-    const tempEl = document.createElement('div');
-    tempEl.style.visibility = 'hidden';
-    tempEl.style.overflow = 'scroll';
-  
-    document.body.appendChild(tempEl);
-
-    const tempElChild = document.createElement('div');
-    tempEl.appendChild(tempElChild);
-
-    const scrollbarWidth = (tempEl.offsetWidth - tempElChild.offsetWidth);
-
-    temEl.parentNode.removeChild(temEl);
-
-    return scrollbarWidth;
-}
-
-const log = document.querySelector('[data-log]');
-log.style.paddingRight = getScrollbarWidth + 'px'
-let itemsLogged = 0
-
-const toLog = function(logMessage) {
-    itemsLogged += 1
-
-    const logItem = document.createElement('span');
-    logItem.classList.add('log__item')
-
-    logItem.textContent = `${itemsLogged}: ${logMessage}`;
-    
-    log.appendChild(logItem);
-    log.scrollTop = log.scrollHeight
-}
+document.addEventListener('contextmenu', function(){
+    return false;
+});
 
 const radiansFromDegrees = function(degrees) {
     return degrees * (Math.PI / 180);
-}
+};
+
+const capitalizeString = function(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const toggleElementVisibility = function(element) {
+    const elementName = element.classList[0];
+
+    if (element.classList.contains(`${elementName}--hidden`)) {
+        ListInLog(`Reveal ${capitalizeString(elementName)}`);
+        element.classList.remove(`${elementName}--hidden`);
+    } else {
+        ListInLog(`Hide ${capitalizeString(elementName)}`);
+        element.classList.add(`${elementName}--hidden`);
+    }
+};
 
 let mouseDown = 0;
 
 const mouse = {
     x: 0,
     y: 0
-}
+};
 
 const currentMouse = {
     x: 0,
     y: 0
-}
+};
 
 const prevMouse = {
     x: 0,
     y: 0
-}
+};
 
 let divisions = 24;
 let angle = 360 / divisions;
@@ -63,14 +50,60 @@ let angle = 360 / divisions;
 let guides = true;
 let mirrorMode = false;
 let particleMode = false;
+let breakMe = false;
+
+const nav = document.querySelector('[data-nav]');
+
+const log = document.querySelector('[data-log]')
+const logList = document.querySelector('[data-log-list]');
+let itemsLogged = 0;
+
+const ListInLog = function(logMessage) {
+    itemsLogged += 1;
+
+    const logListItem = document.createElement('span');
+    logListItem.classList.add('log__item');
+
+    logListItem.textContent = `${itemsLogged}: ${logMessage}`;
+    
+    logList.appendChild(logListItem);
+    logList.scrollTop = logList.scrollHeight;
+}
+
+const about = document.querySelector('[data-about]');
+
+const menuToggleNav = document.querySelector('[data-menu-toggle-nav]');
+const menuToggleLog = document.querySelector('[data-menu-toggle-log]');
+const menuToggleAbout = document.querySelector('[data-menu-toggle-about]');
+const aboutX = document.querySelector('[data-about-x]');
+
+menuToggleNav.addEventListener('click', function() {
+    toggleElementVisibility(nav);
+});
+
+menuToggleLog.addEventListener('click', function() {
+    toggleElementVisibility(log);
+});
+
+menuToggleAbout.addEventListener('click', function() {
+    toggleElementVisibility(about);
+});
+
+aboutX.addEventListener('click', function() {
+    toggleElementVisibility(about);
+});
 
 document.addEventListener('mousedown', () => {
     ++mouseDown;
-})
+
+    log.style.zIndex = -1
+});
 
 document.addEventListener('mouseup', () => {
     --mouseDown;
-})
+
+    log.style.zIndex = 0
+});
 
 canvas.addEventListener('mousemove', function(event) {
     mouse.x = event.pageX - this.offsetLeft;
@@ -85,22 +118,32 @@ window.addEventListener('resize', () => {
 });
 
 document.addEventListener('keydown', (event) => {
-    console.log(event)
+    ListInLog(`Press ${event.code}`);
 
-    toLog(`Press ${event.code}`)
-})
+    if (event.code === 'Escape') {
+        toggleElementVisibility(nav);
+    }
+
+    if (event.code === 'Backquote') {
+        toggleElementVisibility(log);
+    }
+
+    if (event.code === 'Slash') {
+        toggleElementVisibility(about);
+    }
+});
 
 const mouseX = function(canvas, event) {
     const canvasBorder = canvas.getBoundingClientRect();
 
     return event.clientX - canvasBorder.left;
-}
+};
 
 const mouseY = function(canvas, event) {
     const canvasBorder = canvas.getBoundingClientRect();
 
     return event.clientY - canvasBorder.top;
-}
+};
 
 const setup = function() {
     canvas.style.width = canvasWidth + 'px';
@@ -117,7 +160,7 @@ const setup = function() {
     context.strokeStyle = '#000000';
 
     context.translate(canvasWidth / 2, canvasHeight / 2);
-}
+};
 
 let actions = [];
 
@@ -138,6 +181,10 @@ const update = function() {
 
     if (mouseDown) {
         actions.push({x: mx, y: my, px: pmx, py: pmy});
+
+        if (breakMe) {
+            console.log(undefinedVar)
+        }
     }
 
     for (let i = 0; i < actions.length; i++) {
@@ -163,7 +210,7 @@ const update = function() {
 
     prevMouse.x = currentMouse.x;
     prevMouse.y = currentMouse.y;
-}
+};
 
 setup();
 update();
